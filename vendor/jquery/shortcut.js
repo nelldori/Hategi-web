b87e75a4f92ddfb616f06c8177aa44196a95e1de
thvldk913 <thvldk913@naver.com>
2018-12-08 06:23:51 +0900

    Update shortcut.js

--- vendor/jquery/shortcut.js
+++ vendor/jquery/shortcut.js
@@ -58,7 +58,7 @@ jQuery(document).ready(function() {
  // executes when HTML-Document is loaded and DOM is ready
 console.log("document is ready");
 
-  jQuery('.IT_trigger[href^=map.html]').click(function(e){
+  jQuery('.IT_trigger[href^=#]').click(function(e){
     e.preventDefault();
 
     var xCoord = -14.8
